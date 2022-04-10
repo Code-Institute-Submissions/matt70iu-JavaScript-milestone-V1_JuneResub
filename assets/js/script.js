bb6d82da6a098bfb30abf16c5f96ed17d7852976
matt70iu <mattadams60@gmail.com>
2022-04-10 15:45:40 +0000

    define provisional script variables

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+let toDoButton = document.getElementById('add-to-list');
+let toDoContain = document.getElementById('my-to-dos-container');
+let inputField = document.getElementById('list-input-field');
+
+toDoButton.addEventListener

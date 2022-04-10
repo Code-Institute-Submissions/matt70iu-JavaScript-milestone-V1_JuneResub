8b86d43fcca456886d16f89eb9f644a58a3ce57b
matt70iu <mattadams60@gmail.com>
2022-04-10 17:21:56 +0000

    get script working, add items, auto erase input field when adding

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,11 @@ let toDoButton = document.getElementById('add-to-list');
 let toDoContain = document.getElementById('my-to-dos-container');
 let inputField = document.getElementById('list-input-field');
 
-toDoButton.addEventListener
+toDoButton.addEventListener('click', function() {
+    let paragraph = document.createElement('p')
+    paragraph.innerText = inputField.value;
+    toDoContain.appendChild(paragraph);
+    inputField.value = "";
+   
+    
+})
